04f7dce61cd7f91ede6caa66c1855932e94f77c0
Takahiro Abe <hirotaka081630@gmail.com>
2021-01-16 15:04:23 +0900

    2.4

---
+++ README.md
@@ -0,0 +1,8 @@
+# GOプログラミング実践入門
+- https://github.com/mushahiroyuki/gowebprog/blob/master/ch02/chitchat/data/data.go
+
+## メソッド
+特定の型に関連付けられた関数
+
+## ユーティリティ関数
+関数の単なる集まり
---
+++ ch02chitchat/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	threads, err := data.Threads()
+	if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.htnl"}
+		private_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/private.navbar.html",
+			"templates/index.htnl"}
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(pribate_tmpl_files...))
+		}
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
+}
